eb9977450fcd1083618d79f3f14fe1b10aa85b14
kokoakuma <aikw9101@gmail.com>
2023-02-02 14:43:20 +0000

    done list 3-7

--- start/src/main.rs
+++ start/src/main.rs
@@ -1,9 +1,11 @@
-fn myprint<T: std::fmt::Display>(msg: T) {
-    println!("{}", msg)
+fn myprint<T: std::fmt::Display>(msg: &T) {
+    println!("{}", *msg)
 }
 fn main() {
     let s = "Hello".to_string();
-    let ss = s.clone();
-    myprint(s);
-    myprint(ss);
+    let s_ref = &s;
+    let s_ref2 = &s;
+    myprint(&s_ref);
+    myprint(&s_ref2);
+    myprint(&s_ref2);
 }

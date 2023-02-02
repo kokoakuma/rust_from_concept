6848a63f4a1033765d9f7e31dcb5e56d2d0469c2
kokoakuma <aikw9101@gmail.com>
2023-02-02 15:04:03 +0000

    done lifetime

--- start/src/main.rs
+++ start/src/main.rs
@@ -1,15 +1,8 @@
-fn myclear(x: &mut String) {
-    x.clear();
-}
 fn main() {
-    let mut s = "Hello".to_string();
-    println!("s={}", s);
-
-    let s_ref = &mut s;
-    myclear(s_ref);
-    println!("s={}", s);
-
-    let s_ref2 = &mut s;
-    myclear(s_ref2);
-    println!("s={}", s);
+    let x;
+    {
+        let y = 1;
+        x = &y;
+    }
+    println!("{}", x);
 }
